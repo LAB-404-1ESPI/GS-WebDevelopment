a386bc29659b52635d454441ea14da56743994ab
Arthur Fiorese <arthur.herrada@gmail.com>
2026-06-07 23:18:44 -0300

    feat: animação do header do projeto

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,14 @@
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 20) {
+        header.classList.add('rolado');
+    } else {
+        header.classList.remove('rolado');
+    }
+});
+ 
+ 
 
  const perguntas = [
 
